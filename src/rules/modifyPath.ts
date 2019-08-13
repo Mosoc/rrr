de393166d437c7b98c4d5fa12c3248ab2ba191f3
Bruno Anderson Chen <mosoc@colle.co>
2019-08-13 14:33:20 +0900

    Pass paths to modifyPath

--- src/rules/modifyPath.ts
+++ src/rules/modifyPath.ts
@@ -1,7 +1,16 @@
-import { HttpRequestMethod, HttpRequestObject} from '../types';
+import { HttpRequestMethod, HttpRequestObject } from '../types';
 
-const modifyPath = (method: HttpRequestMethod) => (input: HttpRequestObject): HttpRequestObject => {
-  if(input.method !== method ) { return input; }
+const modifyPath = (
+  method: HttpRequestMethod,
+  selectedPath: string,
+  distinationPath: string
+) => (input: HttpRequestObject): HttpRequestObject => {
+  if (selectedPath === distinationPath) {
+    return input;
+  }
+  if (input.method !== method) {
+    return input;
+  }
   return input;
 };
 
