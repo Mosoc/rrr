6cc3559877c802892bb03ecfd23a6e36bd6d68df
Bruno Anderson Chen <mosoc@colle.co>
2019-08-15 10:52:13 +0900

    Check timestamp's format

--- src/rules/addTimestamp.spec.ts
+++ src/rules/addTimestamp.spec.ts
@@ -16,11 +16,24 @@ const requestWithoutTimestamp: HttpRequestObject = {
 
 const addTimestampByExample = addTimestamp(methods, timestampName);
 
+const checkTimestampFormat = (timestamp: any) => {
+  if (timestamp && typeof timestamp === 'string') {
+    // except timestamp === ''
+    new Date(timestamp);
+  } else {
+    throw new TypeError('Timestamp should be string.');
+  }
+};
+
+// expect(() => new Date(typeof result.headers[timestampName] === 'string' ? result.headers[timestampName] : 'Invalid format')).to.not.throw();
 describe('Add Timestamp', () => {
   it('check timestamp format', () => {
     const origin = requestWithoutTimestamp;
     const result = addTimestampByExample(origin);
     expect(result.headers.hasOwnProperty(timestampName)).to.be.true;
     expect(result.headers[timestampName]).to.a('string');
+    expect(() =>
+      checkTimestampFormat('result.headers[timestampName]')
+    ).to.not.throw();
   });
 });
