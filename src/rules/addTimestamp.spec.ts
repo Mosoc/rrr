2e8bcec8113e2b45a1b43f74108509ddf0a47055
Bruno Anderson Chen <mosoc@colle.co>
2019-08-15 18:45:21 +0900

    Use Regular expression to test ISO-8601:2004

--- src/rules/addTimestamp.spec.ts
+++ src/rules/addTimestamp.spec.ts
@@ -19,7 +19,11 @@ const addTimestampByExample = addTimestamp(methods, timestampName);
 const checkTimestampFormat = (timestamp: any) => {
   if (timestamp && typeof timestamp === 'string') {
     // except timestamp === ''
-    new Date(timestamp);
+    const ISO8601:RegExp = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/;
+    // https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetimes
+    if (!ISO8601.test(timestamp)) {
+      throw new TypeError('Timestamp should use ISO-8601:2004.');
+    }
   } else {
     throw new TypeError('Timestamp should be string.');
   }
@@ -33,7 +37,7 @@ describe('Add Timestamp', () => {
     expect(result.headers.hasOwnProperty(timestampName)).to.be.true;
     expect(result.headers[timestampName]).to.a('string');
     expect(() =>
-      checkTimestampFormat('result.headers[timestampName]')
+      checkTimestampFormat(result.headers[timestampName])
     ).to.not.throw();
   });
 });
