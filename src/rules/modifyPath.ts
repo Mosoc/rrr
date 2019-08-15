464879e7a2e3a116ca6b108bb8d333f32a0a5728
Bruno Anderson Chen <mosoc@colle.co>
2019-08-15 12:38:33 +0900

    Implement  modifyPath

--- src/rules/modifyPath.ts
+++ src/rules/modifyPath.ts
@@ -8,10 +8,19 @@ const modifyPath = (
   if (selectedPath === distinationPath) {
     return input;
   }
+
   if (!methods.includes(input.method)) {
     return input;
   }
-  return input;
+  const url = new URL(input.url);
+
+  if (url.pathname !== selectedPath) {
+    return input;
+  }
+
+  url.pathname = distinationPath;
+  const output = { ...input, url: url.href };
+  return output;
 };
 
 export default modifyPath;
