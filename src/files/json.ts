c3d13caefa17f7e389652215341fc02297c6292b
Bruno Anderson Chen <mosoc@colle.co>
2019-08-16 11:48:11 +0900

    Create json IO and test-file folder

---
+++ src/files/json.ts
@@ -0,0 +1,40 @@
+// tslint:disable: no-console
+// command: ts-node src/files/json.ts
+import fs from 'fs-extra';
+fs.readFile(
+  './test-file/source.json',
+  'utf-8',
+  (error: NodeJS.ErrnoException, data: string) => {
+
+    if (error) {
+      console.log(error);
+    }
+
+    console.log(data);
+    const dataObject = JSON.parse(data);
+    console.log(dataObject);
+
+    // Almost the same as writeFile, except that if the directory does not exist, it's created.
+    fs.outputFile('./test-file/copy-dist.json', data, () => {
+      return;
+    });
+
+    dataObject.url = 'https://www.shopback.com/shopback/me';
+
+    fs.outputFile(
+      './test-file/copy-modified.json',
+      JSON.stringify(dataObject),
+      () => {
+        return;
+      }
+    );
+    fs.outputJSON('./test-file/copy-modified-json.json', dataObject);
+  }
+);
+
+fs.readJson('./test-file/source.json', (error: Error, packageObj: any) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(packageObj.name);
+});
