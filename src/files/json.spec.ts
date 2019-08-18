556450bccc01c18ee6d01e2ea410e3fc7b6b25c1
Bruno Anderson Chen <mosoc@colle.co>
2019-08-18 21:20:39 +0900

    Move path definition into `it`

--- src/files/json.spec.ts
+++ src/files/json.spec.ts
@@ -3,11 +3,10 @@ import handleJSON from './json';
 
 import 'mocha';
 
-const inputFilePath = 'test-files/source.json';
-const outputFilePath = 'test-files/output/modified.json';
-
 describe('Test file IO with JSON format', () => {
   it('successful case', () => {
+    const inputFilePath = 'test-files/source.json';
+    const outputFilePath = 'test-files/output/modified.json';
     expect(() => handleJSON()(inputFilePath, outputFilePath)).to.not.throw();
   });
 });
--- src/files/yaml.spec.ts
+++ src/files/yaml.spec.ts
@@ -3,11 +3,10 @@ import handleYAML from './yaml';
 
 import 'mocha';
 
-const inputFilePath = 'test-files/source.yaml';
-const outputFilePath = 'test-files/output/modified.yaml';
-
 describe('Test file IO with YAML format', () => {
   it('successful case', () => {
+    const inputFilePath = 'test-files/source.yaml';
+    const outputFilePath = 'test-files/output/modified.yaml';
     expect(() => handleYAML()(inputFilePath, outputFilePath)).to.not.throw();
   });
 });
