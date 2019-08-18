bffe301a87cd57eec2a53ca20f0739f51055372a
Bruno Anderson Chen <mosoc@colle.co>
2019-08-18 23:37:31 +0800

    Remove comment

--- examples/cli/index.ts
+++ examples/cli/index.ts
@@ -4,7 +4,6 @@ const { defaultRuleSet } = Rules;
 if (process.argv.length > 4) {
   const argv = process.argv.slice(2);
 
-  // $ node index.js test-files/source.json test-files/output/cli.json json
   const inputFilePath = argv[0];
   const outputFilePath = argv[1];
   const fileFormat = argv[2];
